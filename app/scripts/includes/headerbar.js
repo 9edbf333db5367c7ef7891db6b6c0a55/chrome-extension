4417738362d9db4a36da6e4ee74e07559ccfe7e8
Eugene Mutai <eugenemutai@gmail.com>
2017-01-13 15:26:54 +0300

    Refactored header and modal injection code

--- app/scripts/includes/headerbar.js
+++ app/scripts/includes/headerbar.js
@@ -1,46 +1,35 @@
 export default () => {
-  // TO DO: Trottle/Debounce the header clicking to be clicked once every 3 seconds
-  const headerBarOnClick = () => {
-    // check if it's the cart page
-    // if not redirect the user to the card page
-    // wait for it to load
-    // check for the VM hashtag in the page
-    // tell the user VM has began extracting the items in the page
-    // wait for the page to complete loading then start the extraction
-  };
-
   return new Promise((resolve) => {
     // Add the Vitumob header bar to the page
     const headerBarTemplate = chrome.extension.getURL('injectables/header-bar.html');
     $('<div id=\'vitumob-header-bar\' />').prependTo('body').load(headerBarTemplate, () => {
-      const headerBar = $('.vm-header-bar');
-      headerBar.click(headerBarOnClick);
+      const headerBar = $('#vm-header-bar');
+      let stickyHeaderBar = $('#vitumob-fixed-header');
 
       // clone the header bar and append it to the page with additional custom styling
-      const stickyHeaderBar = $('.vitumob-fixed-header');
       if (stickyHeaderBar.length < 1) {
         const cssStyling = {
           position: 'fixed',
           top: 0,
           left: 0,
-          zIndex: 9999,
+          zIndex: 9990,
           width: '100%',
         };
         const stickyHeader = headerBar.clone();
-        stickyHeader.css(cssStyling).addClass('vitumob-fixed-header');
-        $('body').prepend(stickyHeader);
+        stickyHeader.css(cssStyling);
+        stickyHeaderBar = $(stickyHeader).appendTo('body')
+          .wrap('<div id=\'vitumob-fixed-header\'>');
       }
 
       // add scroll behaviour for the sticky headerBar
       $(document).on('scroll', () => {
         if (window.scrollY > headerBar.height()) {
           stickyHeaderBar.show();
-          return;
+        } else {
+          stickyHeaderBar.hide();
         }
-
-        stickyHeaderBar.hide();
       });
-      resolve(null);
+      resolve({ headerBar, stickyHeaderBar });
     });
   });
 };
--- app/scripts/includes/modal.js
+++ app/scripts/includes/modal.js
@@ -2,8 +2,18 @@ export default () => {
   // Add the Vitumob header bar to the page
   return new Promise((resolve) => {
     const modalTemplate = chrome.extension.getURL('injectables/modal.html');
-    $('<div id=\'vitumob-modal\' />').prependTo('body').load(modalTemplate, () => {
-      resolve(null);
+    $('<div id=\'vitumob-modal-box\' />').prependTo('body').load(modalTemplate, () => {
+      $('.vm-okay-button').click(function hideModalFooter() {
+        $(this).parent().parent().hide();
+      });
+
+      $('.vm-cancel-button').click(() => {
+        // TO DO:
+        // revert(remove) the checkout items that were just checked out
+        // get the ORDER ID, get the ITEMS related to the ID and DELETE them
+        // DELETE /order/:orderId
+      });
+      resolve($('#vm-modal-box'));
     });
   });
 };
