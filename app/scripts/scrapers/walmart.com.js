1708af534b470fe9e705a20eb308c11e285bb675
Eugene Mutai <eugenemutai@gmail.com>
2017-03-04 15:28:22 +0300

    Price and quantity walmart fix

--- app/scripts/scrapers/walmart.com.js
+++ app/scripts/scrapers/walmart.com.js
@@ -14,9 +14,14 @@ export default {
 
       item.image = itemElement.find('.asset-container img').attr('src');
       item.link = encodeURI('https://www.walmart.com/search/?query=' + item.name);
-      item.quantity = parseInt(itemElement.find('.chooser-option-current').text(), 10);
+      let quantity = itemElement.find('.Cart-Common-QuantitySelector.quantity-selector select');
+      if (!quantity.val()) {
+        quantity = itemElement.find('.cart-item-quantity span:last-child span:last-child');
+        quantity = quantity.text().trim().replace(/:|\s/g, '');
+      }
+      item.quantity = parseInt(quantity && quantity.val ? quantity.val() : quantity, 10);
 
-      const priceString = itemElement.find('.Price span').text().replace(/\$|,|\s/g, '');
+      const priceString = itemElement.find('.Price > span').text().replace(/\$|,|\s/g, '');
       item.price = parseFloat(priceString, 10) / item.quantity;
       return item;
     });
